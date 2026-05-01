7e3f5e0903c7133a2f703cb263bb9da821a2f779
MohammadImrankhan <31203721+MohammadImrankhan@users.noreply.github.com>
2026-05-01 16:27:52 +0530

    Add files via upload

--- RubaruHillResort/src/components/feedback/Feedback.jsx
+++ RubaruHillResort/src/components/feedback/Feedback.jsx
@@ -2,8 +2,11 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../AuthContext";
 import FeedbackForm from "./FeedbackForm";
 import FeedbackList from "./FeedbackList";
+const API2 = import.meta.env.VITE_API_URL;
 
 export default function Feedback() {
+  const API = import.meta.env.VITE_API_URL;
+
   const { user } = useAuth();
   const [feedbacks, setFeedbacks] = useState([]);
   useEffect(() => {
@@ -13,9 +16,10 @@ export default function Feedback() {
 
   const fetchFeedbacks = async () => {
     try {
-      const res = await fetch(
-        "https://localhost:7037/api/Feedback/get-feedback",
-      );
+      const res = await fetch(`${API}/api/Feedback/get-feedback`);
+      // const res = await fetch(
+      //   "https://localhost:7037/api/Feedback/get-feedback",
+      // );
       const data = await res.json();
       setFeedbacks(data);
     } catch (err) {
