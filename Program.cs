bdc1a406c593ec5b7f068cfad4bbc3c0af4f3e5d
MohammadImrankhan <31203721+MohammadImrankhan@users.noreply.github.com>
2026-05-01 16:23:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+
+namespace RubaruAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowReact",
+                    policy => policy
+                        .WithOrigins("http://localhost:5173", "https://your-vercel-app.vercel.app")
+                           
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+            });
+
+            var app = builder.Build();
+            app.UseCors("AllowReact");
+            app.MapGet("/", () => "Rubaru API Running");
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            app.Urls.Add($"http://0.0.0.0:{port}");
+            // Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+            app.UseSwagger();
+             app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.UseStaticFiles();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
